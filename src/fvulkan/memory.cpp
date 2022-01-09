ed9e1baf015989af284c7873ac43985ae2802f54
Kristopher Pearson <bigskyman02@gmail.com>
2022-01-09 10:11:36 -0500

    add memory allocation checks

--- src/fvulkan/memory.cpp
+++ src/fvulkan/memory.cpp
@@ -5,58 +5,71 @@
 
 namespace fgl::vulkan
 {
+	size_t Buffer::bytecount { 0 }; //Static member of Buffer
+	//BUFFER
+	vk::raii::Buffer Buffer::create_buffer(
+		const Context& vulkan,
+		const vk::DeviceSize size,
+		const vk::BufferUsageFlagBits usageflags,
+		const vk::SharingMode sharingmode ) const
+	{
+		constexpr uint32_t number_of_family_indexes { 1 };
 
-    //BUFFER
-    vk::raii::Buffer Buffer::create_buffer(
-        const Context& vulkan,
-        const vk::DeviceSize size,
-        const vk::BufferUsageFlagBits usageflags,
-        const vk::SharingMode sharingmode ) const
-    {
-        constexpr uint32_t number_of_family_indexes { 1 };
+		const auto index { vulkan.index_of_first_queue_family( vk::QueueFlagBits::eCompute ) };
 
-        const auto index { vulkan.index_of_first_queue_family( vk::QueueFlagBits::eCompute ) };
+		const vk::BufferCreateInfo ci(
+			{},
+			size,
+			usageflags,
+			sharingmode,
+			number_of_family_indexes,
+			&index
+		);
+		return vulkan.device.createBuffer( ci );
+	}
 
-        const vk::BufferCreateInfo ci(
-            {},
-            size,
-            usageflags,
-            sharingmode,
-            number_of_family_indexes,
-            &index
-        );
-        return vulkan.device.createBuffer( ci );
-    }
-
-    //MEMORY
-    std::pair<uint32_t, vk::DeviceSize> Buffer::get_memory_type(
-        const vk::PhysicalDeviceMemoryProperties& device_memory_properties,
-        const vk::MemoryPropertyFlags flags ) const
-    {
-        for(
-            uint32_t current_memory_index = 0;
-            current_memory_index < device_memory_properties.memoryTypeCount;
-            ++current_memory_index )
-        {
-            const vk::MemoryType memoryType {
-                device_memory_properties.memoryTypes[current_memory_index]
-            };
-            if( flags & memoryType.propertyFlags )
-            {
-                const vk::DeviceSize memory_heap_size {
-                     device_memory_properties.memoryHeaps[memoryType.heapIndex].size
-                };
-                return std::pair { current_memory_index, memory_heap_size };
-            }
-        }
-        throw std::runtime_error( "Failed to get memory type." );
-    }
+	//MEMORY
+	std::pair<uint32_t, vk::DeviceSize> Buffer::get_memory_type(
+		const vk::PhysicalDeviceMemoryProperties& device_memory_properties,
+		const vk::MemoryPropertyFlags flags ) const
+	{
+		for(
+			uint32_t current_memory_index = 0;
+			current_memory_index < device_memory_properties.memoryTypeCount;
+			++current_memory_index )
+		{
+			const vk::MemoryType memoryType {
+				device_memory_properties.memoryTypes[current_memory_index]
+			};
+			if( flags & memoryType.propertyFlags )
+			{
+				const vk::DeviceSize memory_heap_size {
+					 device_memory_properties.memoryHeaps[memoryType.heapIndex].size
+				};
+				return std::pair { current_memory_index, memory_heap_size };
+			}
+		}
+		throw std::runtime_error( "Failed to get memory type." );
+	}
 
 	vk::raii::DeviceMemory Buffer::create_device_memory(
-            const Context& context,
-            const vk::MemoryPropertyFlags flags ) const
+		const Context& context,
+		const vk::MemoryPropertyFlags flags ) const
 	{
 		const auto [memindex, size] { get_memory_type( context.physical_device.getMemoryProperties(), flags ) };
+
+		bytecount += bytesize;
+		if( context.properties.limits.maxMemoryAllocationCount < bytecount )
+		{
+			std::stringstream ss;
+			ss << "Attempting to allocate too much memory (in Byte)\n";
+			ss << "\tMemory allocated: " << bytesize << "\n";
+			ss << "\tMaximum Memory: " << context.properties.limits.maxMemoryAllocationCount << "\n";
+			ss << "\tMemory avalilable: " << context.properties.limits.maxMemoryAllocationCount - ( bytecount - bytesize ) << "\n";
+			ss << "\tMemory Over: " << bytecount - context.properties.limits.maxMemoryAllocationCount - ( bytecount - bytesize ) << "\n";
+
+			throw std::runtime_error( ss.str() );
+		}
 
 		const vk::MemoryAllocateInfo memInfo( bytesize, memindex );
 
--- src/fvulkan/memory.hpp
+++ src/fvulkan/memory.hpp
@@ -13,55 +13,67 @@
 namespace fgl::vulkan
 {
 
-    class Buffer
-    {
-    public:
-        uint32_t binding;
+	class Buffer
+	{
+	public:
+		uint32_t binding;
 		vk::DescriptorType buffer_type;
-        vk::DeviceSize bytesize;
-        vk::raii::Buffer buffer;
-        vk::raii::DeviceMemory memory;
+		vk::DeviceSize bytesize;
+		vk::raii::Buffer buffer;
+		vk::raii::DeviceMemory memory;
 
-        Buffer() = delete;
+		//Static memory
+		static size_t bytecount;
 
-        Buffer(
-            const Context& context,
-            const vk::DeviceSize& size,
-            const vk::BufferUsageFlagBits usageflags,
-            const vk::SharingMode sharingmode,
-            const uint32_t _binding,
-            const vk::MemoryPropertyFlags flags,
-            const vk::DescriptorType type )
-            :
-            binding( _binding ),
+		Buffer() = delete;
+
+		Buffer(
+			const Context& context,
+			const vk::DeviceSize& size,
+			const vk::BufferUsageFlagBits usageflags,
+			const vk::SharingMode sharingmode,
+			const uint32_t _binding,
+			const vk::MemoryPropertyFlags flags,
+			const vk::DescriptorType type )
+			:
+			binding( _binding ),
 			buffer_type( type ),
-            bytesize( size ),
-            buffer( create_buffer( context, size, usageflags, sharingmode ) ),
-            memory( create_device_memory( context, flags ) )
-        {
-            buffer.bindMemory( *memory, 0 );
-            std::cout << "Memory created successfully with binding:" << _binding << std::endl;
-        }
+			bytesize( size ),
+			buffer( create_buffer( context, size, usageflags, sharingmode ) ),
+			memory( create_device_memory( context, flags ) )
+		{
+			buffer.bindMemory( *memory, 0 );
+			std::cout << "Memory created successfully with binding:" << _binding << " Allocated " << size << " Bytes" << std::endl;
+		}
 
-        std::pair<uint32_t, vk::DeviceSize> get_memory_type(
-            const vk::PhysicalDeviceMemoryProperties& device_memory_properties,
-            const vk::MemoryPropertyFlags flags
+		Buffer( const Buffer& ) = delete;
+		Buffer( Buffer&& ) = default;
+
+		std::pair<uint32_t, vk::DeviceSize> get_memory_type(
+			const vk::PhysicalDeviceMemoryProperties& device_memory_properties,
+			const vk::MemoryPropertyFlags flags
 		) const;
 
-        vk::raii::Buffer create_buffer(
-            const Context& context,
-            const vk::DeviceSize size,
-            const vk::BufferUsageFlagBits usageflags,
-            const vk::SharingMode sharingmode
+		vk::raii::Buffer create_buffer(
+			const Context& context,
+			const vk::DeviceSize size,
+			const vk::BufferUsageFlagBits usageflags,
+			const vk::SharingMode sharingmode
 		) const;
 
-        vk::raii::DeviceMemory create_device_memory(
-            const Context& context,
-            const vk::MemoryPropertyFlags flags
+		vk::raii::DeviceMemory create_device_memory(
+			const Context& context,
+			const vk::MemoryPropertyFlags flags
 		) const;
 
-        void* get_memory() const;
-    };
+		void* get_memory() const;
+
+		~Buffer()
+		{
+			bytecount -= bytesize;
+
+		}
+	};
 
 }
 
