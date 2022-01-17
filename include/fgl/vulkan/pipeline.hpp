47685f42e5375d3d3c9408862e4caa8df45024a4
Alaestor <alaestor@null.net>
2022-01-17 07:28:24 -0500

    changed to use zip, constrained templates, added attributes

--- include/fgl/vulkan/pipeline.hpp
+++ include/fgl/vulkan/pipeline.hpp
@@ -1,72 +1,33 @@
 #ifndef FGL_VULKAN_PIPELINE_HPP_INCLUDE
 #define FGL_VULKAN_PIPELINE_HPP_INCLUDE
 
+#include <string>
+#include <vector>
+#include <filesystem>
+#include <iostream>
+#include <ranges>
+#include <concepts>
+#include <cassert>
+
 #include <vulkan/vulkan_raii.hpp>
 #include "context.hpp"
 #include "memory.hpp"
 
-#include <string>
-#include <filesystem>
-#include <vector>
-
-#include <iostream>
+#include <fgl/utility/zip.hpp>
 
 namespace fgl::vulkan
 {
 
 	class Pipeline
 	{
-	public:
-
-		vk::raii::ShaderModule shader_module;
-		vk::raii::DescriptorSetLayout descriptor_set_layouts;
-		vk::raii::DescriptorPool pool;
-		vk::raii::PipelineLayout layout;
-		vk::raii::Pipeline pipeline;
-		vk::raii::DescriptorSets sets;
-
-		Pipeline() = delete;
-
-		/// TODO constrain template to range of buffers
-		template <typename T>
-		Pipeline(
-			const Context& cntx,
-			const std::filesystem::path& shaderpath,
-			const std::string& shader_init_name,
-			const T& buffers )
-			:
-			shader_module( create_shader_module( cntx, shaderpath ) ),
-			descriptor_set_layouts( create_descriptor_set_layout( cntx, buffers ) ),
-			pool( create_descriptor_pool( cntx, buffers ) ),
-			layout( create_pipeline_layout( cntx ) ),
-			pipeline( create_pipeline( cntx, shader_init_name ) ),
-			sets( create_descriptor_sets( cntx ) )
-		{
-			std::vector<vk::WriteDescriptorSet> writeset;
-			std::vector<vk::DescriptorBufferInfo> bufferinfo;
-
-			writeset.resize( buffers.size() );
-			bufferinfo.resize( buffers.size() );
-
-			for( size_t i = 0; i < buffers.size(); ++i )
-			{
-				constexpr vk::DescriptorImageInfo* imgpointer { nullptr };
-				bufferinfo.at( i ) = vk::DescriptorBufferInfo( *buffers.at( i ).buffer, 0, buffers.at( i ).bytesize );
-				writeset.at( i ) = vk::WriteDescriptorSet( *sets.front(), buffers.at( i ).binding, 0, 1, buffers.at( i ).buffer_type, imgpointer, &bufferinfo.at( i ) );
-			}
-
-			cntx.device.updateDescriptorSets( writeset, nullptr );
-			std::cout << "Pipeline made successfuly with " << buffers.size() << " buffers." << std::endl;
-		}
-
-		vk::raii::ShaderModule create_shader_module(
+		[[nodiscard]] vk::raii::ShaderModule create_shader_module(
 			const Context& cntx,
 			const std::filesystem::path path
 		) const;
 
-		/// TODO constrain template to range of buffers
-		template <typename T>
-		vk::raii::DescriptorSetLayout create_descriptor_set_layout(
+		template <std::ranges::forward_range T>
+		requires std::same_as<std::ranges::range_value_t<T>, fgl::vulkan::Buffer>
+		[[nodiscard]] vk::raii::DescriptorSetLayout create_descriptor_set_layout(
 			const Context& cntx,
 			const T& buffers )
 		{
@@ -89,16 +50,18 @@ namespace fgl::vulkan
 			return vk::raii::DescriptorSetLayout( cntx.device, ci );
 		}
 
-		/// TODO constrain template to range of buffers
-		template <typename T>
-		vk::raii::DescriptorPool create_descriptor_pool(
+		template <std::ranges::forward_range T>
+		requires std::same_as<std::ranges::range_value_t<T>, fgl::vulkan::Buffer>
+		[[nodiscard]] vk::raii::DescriptorPool create_descriptor_pool(
 			const Context& cntx,
 			const T& buffers ) const
 		{
-			const auto count = static_cast< uint32_t >( buffers.size() );
-			const vk::DescriptorPoolSize poolsize { buffers.front().buffer_type, count };
 			constexpr uint32_t max_sets{ 1 };
 			constexpr uint32_t pool_size_count{ 1 };
+			const vk::DescriptorPoolSize poolsize {
+				std::ranges::begin(buffers)->buffer_type,
+				static_cast< uint32_t >( std::ranges::size(buffers) )
+			};
 			const vk::DescriptorPoolCreateInfo ci(
 				vk::DescriptorPoolCreateFlagBits::eFreeDescriptorSet,
 				max_sets,
@@ -108,14 +71,82 @@ namespace fgl::vulkan
 			return vk::raii::DescriptorPool( cntx.device, ci );
 		}
 
+		[[nodiscard]]
 		vk::raii::PipelineLayout create_pipeline_layout( const Context& cntx ) const;
 
-		vk::raii::Pipeline create_pipeline(
+		[[nodiscard]] vk::raii::Pipeline create_pipeline(
 			const Context& cntx,
 			const std::string init_function_name
 		) const;
 
+		[[nodiscard]]
 		vk::raii::DescriptorSets create_descriptor_sets( const Context& cntx ) const;
+
+	public:
+
+		vk::raii::ShaderModule shader_module;
+		vk::raii::DescriptorSetLayout descriptor_set_layouts;
+		vk::raii::DescriptorPool pool;
+		vk::raii::PipelineLayout layout;
+		vk::raii::Pipeline pipeline;
+		vk::raii::DescriptorSets sets;
+
+		Pipeline() = delete;
+
+		template <std::ranges::forward_range T>
+		requires std::same_as<std::ranges::range_value_t<T>, fgl::vulkan::Buffer>
+		[[nodiscard]] explicit
+		Pipeline(
+			const Context& cntx,
+			const std::filesystem::path& shaderpath,
+			const std::string& shader_init_name,
+			const T& buffers )
+		:
+			shader_module( create_shader_module( cntx, shaderpath ) ),
+			descriptor_set_layouts( create_descriptor_set_layout( cntx, buffers ) ),
+			pool( create_descriptor_pool( cntx, buffers ) ),
+			layout( create_pipeline_layout( cntx ) ),
+			pipeline( create_pipeline( cntx, shader_init_name ) ),
+			sets( create_descriptor_sets( cntx ) )
+		{
+			std::vector<vk::WriteDescriptorSet> writeset;
+			std::vector<vk::DescriptorBufferInfo> bufferinfo;
+
+			const auto number_of_buffers{ std::ranges::size(buffers) };
+			writeset.resize( number_of_buffers );
+			bufferinfo.resize( number_of_buffers );
+
+			/// magic values
+			constexpr uint32_t offset { 0 };
+			constexpr vk::DescriptorImageInfo* imgpointer { nullptr };
+			constexpr uint32_t array_element{ 0 };
+			constexpr uint32_t descriptor_count{ 1 };
+
+			/* Simultaneously iterate over buffers, bufferinfo, and writeset.
+				Constructs and assigns elements which are associated by order.
+				Both writeset and info are constructed from a buffer.
+				writeset requires the info's address.*/
+			for (const auto& [buffer, buffer_info, write_set]
+				: fgl::zip(buffers, bufferinfo, writeset))
+			{
+				buffer_info = vk::DescriptorBufferInfo(
+					*buffer.buffer, offset, buffer.bytesize
+				);
+
+				write_set = vk::WriteDescriptorSet(
+					*sets.front(),
+					buffer.binding,
+					array_element,
+					descriptor_count,
+					buffer.buffer_type,
+					imgpointer,
+					&buffer_info
+				);
+			}
+
+			cntx.device.updateDescriptorSets( writeset, nullptr );
+			std::cout << "\n\tConstructed Pipeline with " << buffers.size() << " buffers." << std::endl;
+		}
 	};
 
 
